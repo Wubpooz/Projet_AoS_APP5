4d6884181f0459cbccd20fb28b867b37315da748
Wubpooz <mathieu.waharte@gmail.com>
2026-02-11 19:53:38 +0100

    removed todo

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -182,7 +182,4 @@ userRoutes.get(
     }
     return c.json({ collections }, 200);
   }
-)
-
-
-// TODO POST /:userId/invite/:collectionId - Invite a user to collaborate on a collection (by user ID and collection ID)
+);
